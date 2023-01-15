56de3aa323a901531a6ec816869639ac25496d19
carlquin <cquintej@emeal.nttdata.com>
2023-01-15 18:53:07 -0300

    Refactors

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -1,22 +1,42 @@
-(() => {
-  "use strict";
+const myModule = (() => {
+  ("use strict");
   let deck = [];
-  const types = ["C", "D", "H", "S"];
-  const specials = ["J", "Q", "K", "A"];
+  const types = ["C", "D", "H", "S"],
+    specials = ["J", "Q", "K", "A"];
 
-  let playerPoints = 0,
-    computerPoints = 0;
+  let playersPoints = [];
 
   //Referencais HTML
-  const requestCard = document.querySelector("#requestCard");
-  const newGame = document.querySelector("#newGame");
-  const stopRequest = document.querySelector("#stopRequest");
-  const htmlPoints = document.querySelectorAll("small");
-  const playerCards = document.querySelector("#jugador-cartas");
-  const computerCards = document.querySelector("#computadora-cartas");
+  const requestCard = document.querySelector("#requestCard"),
+    newGame = document.querySelector("#newGame"),
+    stopRequest = document.querySelector("#stopRequest");
+
+  const divPlayersCards = document.querySelectorAll(".divCartas"),
+    htmlPoints = document.querySelectorAll("small");
+
+  //Con esta funcion inicializamos el juego
+  const startGame = (numberOfPlayers = 2) => {
+    console.clear();
+    deck = createDeck();
+    playersPoints = [];
+
+    for (let i = 0; i < numberOfPlayers; i++) {
+      playersPoints.push(0);
+      htmlPoints[i].innerText = 0;
+      divPlayersCards[i].innerHTML = "";
+    }
+
+    //Estas son opciones adicionales para tener en cuenta en el proyecto
+    // htmlPoints.forEach(e => e.innerText = 0)
+    // divPlayersCards.forEach((e) => (e.innerHTML = ""));
+
+    requestCard.disabled = false;
+    stopRequest.disabled = false;
+  };
 
   //Esta funcion crea una nueva baraja mezclada
   const createDeck = () => {
+    deck = [];
     for (let i = 2; i <= 10; i++) {
       for (let type of types) {
         deck.push(i + type);
@@ -28,19 +48,15 @@
       }
     }
 
-    deck = _.shuffle(deck);
-    return deck;
+    return _.shuffle(deck);
   };
 
-  createDeck();
-
   // Esta funcion permite tomar una carta
-  const pickCard = () => {
+  const requestNewCard = () => {
     if (deck.length === 0) {
       alert("No hay mas cartas en la baraja");
     }
-    const card = deck.pop();
-    return card;
+    return deck.pop();
   };
 
   //Con esta funcion le damos valores a las cartas especiales
@@ -50,46 +66,22 @@
     return isNaN(value) ? (value === "A" ? 11 : 10) : value * 1;
   };
 
-  //Events. El segundo parametro del addEventListener es un callback
-  //Player turn
-  requestCard.addEventListener("click", () => {
-    const card = pickCard();
-    playerPoints = playerPoints + cardValue(card);
-    htmlPoints[0].innerText = playerPoints;
+  //Turn: 0 = first player and the last was the computer
+  const accumulatePoints = (turn, card) => {
+    playersPoints[turn] = playersPoints[turn] + cardValue(card);
+    htmlPoints[turn].innerText = playersPoints[turn];
+    return playersPoints[turn];
+  };
 
+  const createCard = (card, turn) => {
     const imgCard = document.createElement("img");
     imgCard.src = `assets/cartas/${card}.png`;
     imgCard.classList.add("carta");
-    playerCards.append(imgCard);
+    divPlayersCards[turn].append(imgCard);
+  };
 
-    if (playerPoints > 21) {
-      console.warn("You lose!");
-      requestCard.disabled = true;
-      computerTurn(playerPoints);
-    } else if (playerPoints === 21) {
-      console.warn("21, great!");
-      requestCard.disabled = true;
-      stopRequest.disabled = true;
-      computerTurn(playerPoints);
-    }
-  });
-
-  // Computer turn
-  const computerTurn = (minPoints) => {
-    do {
-      const card = pickCard();
-      computerPoints = computerPoints + cardValue(card);
-      htmlPoints[1].innerText = computerPoints;
-
-      const imgCard = document.createElement("img");
-      imgCard.src = `assets/cartas/${card}.png`;
-      imgCard.classList.add("carta");
-      computerCards.append(imgCard);
-
-      if (minPoints > 21) {
-        break;
-      }
-    } while (computerPoints < minPoints && minPoints <= 21);
+  const determineWinner = () => {
+    const [minPoints, computerPoints] = playersPoints;
     setTimeout(() => {
       computerPoints === minPoints
         ? alert("Es un empate, nadie gana")
@@ -101,26 +93,50 @@
     }, 200);
   };
 
+  //Events. El segundo parametro del addEventListener es un callback
+  //Player turn
+  requestCard.addEventListener("click", () => {
+    const card = requestNewCard();
+    const playerPoints = accumulatePoints(0, card);
+    createCard(card, 0);
+
+    if (playerPoints > 21) {
+      console.warn("You lose!");
+      requestCard.disabled = true;
+      stopRequest.disabled = true;
+      computerTurn(playerPoints);
+    } else if (playerPoints === 21) {
+      console.warn("21, great!");
+      requestCard.disabled = true;
+      stopRequest.disabled = true;
+      computerTurn(playerPoints);
+    }
+  });
+
+  // Computer turn
+  const computerTurn = (minPoints) => {
+    let computerPoints = 0;
+    do {
+      const card = requestNewCard();
+      const turn = playersPoints.length - 1;
+
+      computerPoints = accumulatePoints(turn, card);
+      createCard(card, turn);
+    } while (computerPoints < minPoints && minPoints <= 21);
+    determineWinner();
+  };
+
   stopRequest.addEventListener("click", () => {
     requestCard.disabled = true;
     stopRequest.disabled = true;
-    computerTurn(playerPoints);
+    computerTurn(playersPoints[0]);
   });
 
   newGame.addEventListener("click", () => {
-    console.clear();
-    deck = [];
-    deck = createDeck();
-    playerPoints = 0;
-    computerPoints = 0;
-
-    htmlPoints[0].innerText = 0;
-    htmlPoints[1].innerText = 0;
-
-    playerCards.innerHTML = "";
-    computerCards.innerHTML = "";
-
-    requestCard.disabled = false;
-    stopRequest.disabled = false;
+    startGame();
   });
+
+  return {
+    newGame: startGame,
+  };
 })();
